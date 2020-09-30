b2c9a18b4ada1bf103fe720129275e86f4c147e4
JobMoreno-UDG <job.moreno7426@alumnos.udg.mx>
2020-09-30 18:55:42 -0500

    numero_e 1.0

--- main.go
+++ main.go
@@ -2,6 +2,21 @@ package main
 
 import "fmt"
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var valor, aux int
+	var num float64
+	fmt.Print("Introduce el limite ")
+	fmt.Scan(&valor)
+	num = 1
+	for i := 0; i < valor; i++ {
+		aux = 1
+		for x := i + 1; x > 1; x-- {
+			aux = aux * x
+		}
+		k:=float64(1)
+		k2:=float64(aux)
+		num = num + k/k2
+	}
+	fmt.Printf("E  %.10f\n",num)
+
 }
